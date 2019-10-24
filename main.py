0840657df3036f5c39121ba61ff2209f50cba55f
Paul Dechorgnat <pdechorgnat@yahoo.fr>
2019-10-24 15:15:36 +0200

    small correction

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def index():
         #saving markdown content
         with open('temp.md', 'w') as file:
             file.write(text)
-        # turning it into htmlhe
+        # turning it into html
         html_from_md = markdown(text, extras=['fenced-code-blocks'])
         sliders = re_md.findall(text)
 
